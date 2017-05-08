4ef86968fdb4c3efab83773f74a3a3c775615c15
aboyer3 <aboyer@dejongrichter.com>
2017-05-08 16:51:24 -0400

    fixedFilePath

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWJveWVyIiwiYSI6ImNpZ3Y1bTN1cTBod2x2cW0zejJyeDRwOWUifQ.pNbGU-s0cDtU_PW9pBsL4w';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v9'
+});
 
+var boroughs = L.geoJson(null, {
+  style: function (feature) {
+    return {
+      color: "black",
+      fill: false,
+      opacity: 1,
+      clickable: false
+    };
+  },
+  onEachFeature: function (feature, layer) {
+    boroughSearch.push({
+      name: layer.feature.properties.BoroName,
+      source: "Boroughs",
+      id: L.stamp(layer),
+      bounds: layer.getBounds()
+    });
+  }
+});
+$.getJSON("assets/geoJSON/boroughs.geojson", function (data) {
+  boroughs.addData(data);
+});
