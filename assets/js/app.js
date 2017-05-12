9d4ca00198c9805cf78fa547b9b2739a853b96aa
aboyer3 <aboyer@dejongrichter.com>
2017-05-12 15:12:41 -0400

    js fiddle

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ map.on('load', function () {
         "type": "fill",
         'source': {
             'type': 'geojson',
-            "data": "assets/geoJSON/blue_bnds.json"
+            "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/blue_bnds.json"
           },
           'paint': {
             'fill-color': '#0BCBF5',
@@ -22,7 +22,7 @@ map.on('load', function () {
 
 
 
-          }},'streets');
+          }});
 
   map.addLayer(
   {
@@ -30,7 +30,7 @@ map.on('load', function () {
     "type": "fill",
     'source': {
         'type': 'geojson',
-        "data": "assets/geoJSON/orange_bnds.json"
+        "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/orange_bnds.json"
       },
       'paint': {
         'fill-color': '#34414F',
@@ -44,7 +44,7 @@ map.on('load', function () {
         "type": "fill",
         'source': {
             'type': 'geojson',
-            "data": "assets/geoJSON/green_bnds.json"
+            "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/green_bnds.json"
           },
           'paint': {
             'fill-color': '#C0EF74',
@@ -58,7 +58,7 @@ map.on('load', function () {
             "type": "fill",
             'source': {
                 'type': 'geojson',
-                "data": "assets/geoJSON/purp_bnds.json"
+                "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/purp_bnds.json"
               },
               'paint': {
                 'fill-color': '#4AAAA7',
@@ -72,7 +72,7 @@ map.on('load', function () {
                 "type": "fill",
                 'source': {
                     'type': 'geojson',
-                    "data": "assets/geoJSON/red_bnds.json"
+                    "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/red_bnds.json"
                   },
                   'paint': {
                     'fill-color': '#ED1D26',
@@ -86,28 +86,143 @@ map.on('load', function () {
                     "type": "fill",
                     'source': {
                         'type': 'geojson',
-                        "data": "assets/geoJSON/yellow_bnds.json"
+                        "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/yellow_bnds.json"
                       },
                       'paint': {
                         'fill-color': '#7D7F81',
                         'fill-opacity': 0.5
 
                       }});
-                      map.addLayer(
-                      {
-                        "id": "reg_school",
-                        "type": "Point",
-                        'source': {
-                            'type': 'geojson',
-                            "data": "assets/geoJSON/reg_school.json"
-                          },
-                          'paint': {
-                            'fill-color': 'red'
 
-                          }});
+                      map.addLayer(
+                  {
+                    "id": "reg-school",
+                    "type": "symbol",
+                    'source': {
+                        'type': 'geojson',
+                        "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/reg_school.json"
+                      },
+                        "layout": {
+            "icon-image": "marker-15",
+            "text-field": "{schnam}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        },"paint": {
+                    "text-color": "#202",
+                    "text-halo-color": "#fff",
+                    "text-halo-width": 2
+                }
+              });
+
+                map.addLayer(
+                  {
+                    "id": "charter-school",
+                    "type": "symbol",
+                    'source': {
+                        'type': 'geojson',
+                        "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/charter_school.json"
+                      },
+                        "layout": {
+            "icon-image": "marker-15",
+            "text-field": "{schnam}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+              });
+                     map.addLayer(
+                  {
+                    "id": "sped-school",
+                    "type": "symbol",
+                    'source': {
+                        'type': 'geojson',
+                        "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/sped_school.json"
+                      },
+                        "layout": {
+            "icon-image": "marker-15",
+            "text-field": "{schnam}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+              });
+
+           map.addLayer(
+                  {
+                    "id": "alt-school",
+                    "type": "symbol",
+                    'source': {
+                        'type': 'geojson',
+                        "data": "https://raw.githubusercontent.com/aboyer3/Oakland_Map/master/assets/geoJSON/alt_school.json"
+                      },
+                        "layout": {
+            "icon-image": "marker-15",
+            "text-field": "{schnam}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+              });
+
+map.on('click', 'blue-bnds', function (e) {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.SCHNAME+" Boundary")
+            .addTo(map);
+    });
+
 
       });
 
+      var toggleableLayerIds = [ 'alt-school', 'charter-school','reg-school','sped-school' ];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
+
+
+
+    /*geojson.source.features.forEach(function(marker) {
+    // create a DOM element for the marker
+    var el = document.createElement('div');
+    el.className = 'marker';
+    el.style.width = marker.properties.iconSize[0] + 'px';
+    el.style.height = marker.properties.iconSize[1] + 'px';
+
+    el.addEventListener('click', function() {
+        window.alert(marker.properties.schnam);
+    });
+
+    // add marker to map
+    new mapboxgl.Marker(el, {offset: [-marker.properties.iconSize[0] / 2, -marker.properties.iconSize[1] / 2]})
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+});*/
 
 /*
       var boroughs = L.geoJson(null, {
